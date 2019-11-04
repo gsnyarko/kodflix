2fb545b404aa55f9696efdbf593d402c49059f74
GIDEON NYARKO <gideonnyarko@HP9C1D7D.localdomain>
2019-11-04 16:23:23 +0000

    task 1 to 5

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import caduta from './images/caduta-pic.jpg';
+import crozza from './images/crozza-pic.jpg';
+import chapo from './images/el chapo-pic.jpg';
+import iene from './images/iene-pic.jpeg';
+import sherlock from './images/sherlock-pic.jpg';
+import striscia from './images/striscia-pic.jpg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to my world
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+        <br /><br></br>
+        <div className='container'>
+          <div className='item'>
+            <img src={caduta} alt='caduta-pic' />
+            <div className='overlay'>
+              <h1>caduta libera</h1>
+            </div>
+          </div>
+          <div className='item'>
+            <img src={crozza} alt='crozza-pic' />
+            <div className='overlay'>
+              <h1>crozza</h1>
+            </div>
+          </div>
+          <div className='item'>
+            <img src={chapo} alt='el chapo-pic' />
+
+            <div className='overlay'>
+              <h1>el chapo</h1>
+            </div>
+          </div>
+        </div>
+        <div className='container'>
+          <div className='item'>
+            <img src={iene} alt='iene-pic' />
+            <div className='overlay'>
+              <h1>le iene</h1>
+            </div>
+          </div>
+          <div className='item'>
+            <img src={sherlock} alt='sherlock-pic' />
+            <div className='overlay'>
+              <h1>sherlock</h1>
+            </div>
+          </div>
+          <div className='item'>
+            <img src={striscia} alt='striscia-pic' />
+            <div className='overlay'>
+              <h1>striscia</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default App; {
+  
+}
