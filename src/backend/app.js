2701465f5550851693f497b448528f540b56240d
GIDEON NYARKO <gideonnyarko@HP9C1D7D.localdomain>
2019-11-27 12:18:27 +0000

    test #2 for deployment

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,6 +1,7 @@
 
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const path = require('path');
 const port = process.env.PORT || 3001;
 const getData = require('./data');
 
